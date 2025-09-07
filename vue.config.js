1033dd92d1542b12138cfc57a584eca7981cb96e
Mukhammadumid Toshmamatov <mamidus@wlan-su-10-201-3-248.local.su.se>
2025-09-07 17:09:11 +0200

    Additions

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const {defineConfig} = require("@vue/cli-service");
 
 module.exports = defineConfig({
     transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === "production" ? "/uniflow/" : "/",
+
     // css: {
     //     extract: true,
     // },
