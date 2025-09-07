46d6b9e32e9ef7afe58585f5aaa6a98bc89a3f1f
Mukhammadumid Toshmamatov <mamidus@Mukhammadumids-MacBook-Pro.local>
2025-09-07 17:13:40 +0200

    Additions

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const {defineConfig} = require("@vue/cli-service");
 
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === "production" ? "/uniflow/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/aviafly/" : "/",
 
     // css: {
     //     extract: true,
